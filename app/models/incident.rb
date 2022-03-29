bbe7e2d7dbdfc47774ac87978b2816a0804ef2d2
Christina Sugiono <christina.sugiono95@gmail.com>
2022-03-29 11:34:57 +0800

    validation incidents

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,3 +1,9 @@
 class Incident < ApplicationRecord
+  TYPE = ["Attempt", "Item Stolen"]
   belongs_to :user
+  has_many_attached :photos
+
+  validates :address, :time, :date, :type, :authorities, :status, presence: true
+  validates :description, length: { maximum: 150 }
+  validates :type, inclusion: { in: TYPE }
 end
