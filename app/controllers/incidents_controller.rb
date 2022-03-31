98ea120f0fcd6b2756a007fe8da799c6139ce246
melikegundogdu <gundogdu.mel@gmail.com>
2022-03-31 14:52:09 +0800

    seed added

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,5 +1,17 @@
 class IncidentsController < ApplicationController
   def index
     @incidents = Incident.all
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @incidents.geocoded.map do |incident|
+      {
+        lat: incident.latitude,
+        lng: incident.longitude
+      }
+    end
+  end
+
+  def show
+    @incident = Incident.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+Incident.destroy_all
+User.destroy_all
+
+# User seed to test geocode
+melike = User.create!(email: "melike@gmail.com", username: "Melike", password: "123456")
+christina = User.create!(email: "christina@gmail.com", username: "Christina", password: "123456")
+joana = User.create!(email: "joana@gmail.com", username: "Joana", password: "123456")
+
+# Incident seed to test geocode
+
+incident_1 = Incident.create!(user: User.first, category: "Item Stolen", authorities: "reported", status: "pending", address: "Canggu, Bali", date: Date.today, description: "in umalas", time: "8:00")
