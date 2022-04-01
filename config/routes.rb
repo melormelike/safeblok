3f618dd23c2999dec870cdb2979159cfc027c193
Christina Sugiono <christina.sugiono95@gmail.com>
2022-04-01 11:34:58 +0800

    resolve routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-
-  resources :incidents, only: [:new, :create]
-=======
   resources :incidents
->>>>>>> 4ef0b393b122416cdb005b12fb848e70bbeec1f3
 end
