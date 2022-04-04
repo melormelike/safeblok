0adbad460e79de45baa1a13ab0517f3768ee066b
melikegundogdu <gundogdu.mel@gmail.com>
2022-04-04 14:55:50 +0800

    info window and photos are attached

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,14 @@ joana = User.create!(email: "joana@gmail.com", username: "Joana", password: "123
 
 # Incident seed to test geocode
 
-incident_1 = Incident.create!(user: User.first, category: "Item Stolen", authorities: "reported", status: "pending", address: "Canggu, Bali", date: Date.today, description: "in umalas", time: "8:00")
-incident_2 = Incident.create!(user: User.last, category: "Item Stolen", authorities: "reported", status: "pending", address: "Ubud, Bali", date: Date.today, description: "in umalas", time: "8:00")
+incident_1 = Incident.create!(user: User.first, category: "Item Stolen", authorities: "reported", status: "pending", address: "Canggu, Bali", date: Date.today, description: "Someone stole my phone while I was riding my scooter. The phone was in the pocket of bike. It was dark so I didn't see how they look like.", time: "20:00")
+incident_2 = Incident.create!(user: User.last, category: "Item Stolen", authorities: "reported", status: "pending", address: "Ubud, Bali", date: Date.today, description: "While I was in the villa, two guys jumped into the garden and stole my laptop which was in the sunbed by the pool. ", time: "8:00")
+
+file_1 = File.open(Rails.root.join("app/assets/images/pin.jpg"))
+file_2 = File.open(Rails.root.join("app/assets/images/pin.jpg"))
+incident_1.photos.attach(io: file_1, filename:'nes.png', content_type: 'image/png')
+incident_1.photos.attach(io: file_2, filename:'nes.png', content_type: 'image/png')
+file_3 = File.open(Rails.root.join("app/assets/images/yellowpin.jpg"))
+file_4 = File.open(Rails.root.join("app/assets/images/yellowpin.jpg"))
+incident_2.photos.attach(io: file_3, filename:'nes.png', content_type: 'image/png')
+incident_2.photos.attach(io: file_4, filename:'nes.png', content_type: 'image/png')
