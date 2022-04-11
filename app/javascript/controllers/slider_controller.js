80fe3d411b347f449f23a0c12f27b4df7b3b1ccb
melikegundogdu <gundogdu.mel@gmail.com>
2022-04-11 16:46:35 +0800

    toggle on the map

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,13 +1,16 @@
 
 import { Controller } from "stimulus";
 export default class extends Controller {
-  connect() {}
+  connect() {
+
+  }
 }
 
+console.log('hey yo');
 let mainContent = document.querySelector('#main_content');
 let secondaryContent = document.querySelector('#secondary_content');
 
-document.querySelector('.switch input').addEventListener('change', e => {
+document.querySelector('#index-toggle').addEventListener('change', e => {
   mainContent.style.display = e.target.checked ? 'block' : 'none';
   secondaryContent.style.display = e.target.checked ? 'none' : 'block';
 });
