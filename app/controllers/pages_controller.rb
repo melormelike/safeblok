2237914fa0cb36ea63dd266014d1a61d2bf4a1c4
Joana Moufat <joana.moufat@gmail.com>
2022-04-12 11:16:07 +0800

    location axis fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,27 @@
+require "json"
+require "open-uri"
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+
+    apiKey = ENV['MAPBOX_API_KEY']
+
+    @incidents_per_location = Hash.new(0)
     @incidents = Incident.all
     # @incident = Incident.find(params[:id])
     # @address = @incident.address
-    @incidents_per_location = Incident.group(:address).count.sort_by{|_key, value| value}.reverse.first(5).to_h
+
+    @incidents.each do |location|
+      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/#{location.longitude},#{location.latitude}.json?access_token=#{apiKey}"
+      neighborhood = JSON.parse(URI.open(url).read)
+      @incidents_per_location[neighborhood["features"][1]["text"]] += 1
+    end
+    @incidents_per_location = @incidents_per_location.sort_by{|_key, value| value}.reverse.first(3).to_h
+
     @incidents_per_category = Incident.group(:category).count.sort_by{|_key, value| value}.reverse.first(5).to_h
     @incidents_per_time = Incident.group(:time).count.sort_by{|_key, value| value}
     @incidents_per_date = Incident.group(:date).count
-
   end
 end
