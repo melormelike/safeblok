68c5fd21b24b81ec43abc49013ad7e6f1e4e24ba
melikegundogdu <gundogdu.mel@gmail.com>
2022-04-13 15:29:33 +0800

    stats controller and index.html

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :incidents
+
+  get '/stats', to: 'stats#index'
 end
