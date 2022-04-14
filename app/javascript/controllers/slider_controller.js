a1c19bcc941a4904b335f29af1069279db62b784
Joana Moufat <joana.moufat@gmail.com>
2022-04-14 10:42:54 +0800

    stimulus for stats page

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,45 +1,21 @@
-
 import { Controller } from "stimulus";
 export default class extends Controller {
   connect() {
+    console.log("hey yo");
+    let mainContent = document.querySelector("#main_content");
+    let secondaryContent = document.querySelector("#secondary_content");
+    let slider = document.querySelector(".switch span");
 
+    document.querySelector("#optionone").addEventListener("click", (e) => {
+      mainContent.style.display = e.target.checked ? "block" : "none";
+      secondaryContent.style.display = e.target.checked ? "none" : "block";
+      slider.classList.remove("right");
+    });
+
+    document.querySelector("#optiontwo").addEventListener("click", (e) => {
+      mainContent.style.display = e.target.checked ? "none" : "block";
+      secondaryContent.style.display = e.target.checked ? "block" : "none";
+      slider.classList.add("right");
+    });
   }
 }
-
-console.log('hey yo');
-let mainContent = document.querySelector('#main_content');
-let secondaryContent = document.querySelector('#secondary_content');
-let slider = document.querySelector('.switch span');
-
-// document.querySelector('#index-toggle').addEventListener('change', e => {
-//   mainContent.style.display = e.target.checked ? 'block' : 'none';
-//   secondaryContent.style.display = e.target.checked ? 'none' : 'block';
-// });
-
-document.querySelector('#optionone').addEventListener('click', e => {
-  mainContent.style.display = e.target.checked ? 'block' : 'none';
-  secondaryContent.style.display = e.target.checked ? 'none' : 'block';
-  slider.classList.remove('right');
-});
-
-document.querySelector('#optiontwo').addEventListener('click', e => {
-  mainContent.style.display = e.target.checked ? 'none' : 'block';
-  secondaryContent.style.display = e.target.checked ? 'block' : 'none';
-  slider.classList.add('right');
-});
-
-
-// document.querySelectorAll('.switch label').forEach((label) =>  {
-//   label.addEventListener("click", (event) => {
-//     console.log(event.currentTarget)
-//   });
-
-// })
-// $('.switch label').on('click', function(){
-//   var indicator = $(this).parent('.switch').find('span');
-//   if ( $(this).hasClass('right') ){
-// 		$(indicator).addClass('right');
-//   } else {
-//     $(indicator).removeClass('right');
-//   }
-// });
