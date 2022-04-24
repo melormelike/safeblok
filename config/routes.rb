e831f704fc37acba598bc51963537b23429f10d8
melikegundogdu <gundogdu.mel@gmail.com>
2022-04-24 10:36:40 +0800

    listofincidents page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :incidents
+  resources :stats, only: [:index]
 end
